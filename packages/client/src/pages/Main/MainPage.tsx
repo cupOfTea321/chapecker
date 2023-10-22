592708fff124287398534d17a22d7c07147752d7
Alex <ddragon99999@gmail.com>
2023-10-22 23:26:22 +0300

    [game menu] main button component and description of the game added

--- packages/client/src/pages/Main/MainPage.tsx
+++ packages/client/src/pages/Main/MainPage.tsx
@@ -1,12 +1,9 @@
 import React from 'react'
 import { Box, Typography } from '@mui/material'
 import './main.scss'
-import { useNavigate } from 'react-router-dom'
+import MainButton from '../../components/main/MainButton'
+
 const MainPage = () => {
-  const navigate = useNavigate()
-  const handleClick = () => {
-    navigate('start')
-  }
   return (
     <Box
       sx={{
@@ -25,21 +22,48 @@ const MainPage = () => {
         }}>
         Шашки Чапаева
       </Typography>
-      <Box
-        component={'button'}
-        className={'main-btn'}
+      <MainButton to={'start'} text={'Начать игру'} />
+      <MainButton to={'board'} text={'Таблица лидеров'} />
+      <MainButton to={'profile'} text={'Профиль'} />
+      <Typography
+        variant={'h2'}
+        component={'h2'}
         sx={{
-          background: '#769656',
-          border: '1px solid #f8f8f8',
-          borderRadius: '20px',
-          fontSize: '32px',
-          padding: '5px 10px',
+          fontSize: '42px',
+          textAlign: 'center',
+          marginTop: '40px',
+        }}>
+        Игровой процесс
+      </Typography>
+      <Typography
+        variant={'h4'}
+        component={'h4'}
+        sx={{
+          fontSize: '18px',
+          textAlign: 'center',
           marginTop: '20px',
-          cursor: 'pointer',
-        }}
-        onClick={handleClick}>
-        Начать игру
-      </Box>
+          width: '60%',
+        }}>
+        Играет 2 игрока — один за "белых", второй за "черных". В начале партии
+        на шахмтной доске 8x8 в первом ряду располагается 8 белых фишек, в
+        восьмом ряду — 8 черных фишек Каждый из игроков, начиная с игрока за
+        "белых", по очереди проделывает следующие действия в указанной
+        последовательности:
+        <br /> <br />
+        Выбирает фишку, которой будет ходить
+        <br />
+        Указывает направление, в котором полетит фишка
+        <br />
+        Указывает силу, с которой будет брошена фишка
+        <br />
+        <br />
+        После этого выбранная фишка бросается в указанном направлении с
+        указанной силой. Она соударется с другими фишками и может выкинуть их за
+        пределы игрового поля. Когда все фишки остановились, ход переходит
+        следующему игроку Игра продолжается до тех пор, пока все фишки хотя бы
+        одного из игроков не окажутся выбитыми за пределы игрового поля.
+        Победителем считается игрок, сделавший последний ход.
+      </Typography>
     </Box>
   )
 }
