a65118eb7cd098875da5c1d4fbe3cda0d5af0e11
Alex <ddragon99999@gmail.com>
2023-10-22 22:51:06 +0300

    [page] main demo

--- packages/client/src/pages/Main/MainPage.tsx
+++ packages/client/src/pages/Main/MainPage.tsx
@@ -1,7 +1,47 @@
 import React from 'react'
-
+import { Box, Typography } from '@mui/material'
+import './main.scss'
+import { useNavigate } from 'react-router-dom'
 const MainPage = () => {
-  return <div>MainPage</div>
+  const navigate = useNavigate()
+  const handleClick = () => {
+    navigate('start')
+  }
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+      className={'container'}>
+      <Typography
+        variant={'h1'}
+        component={'h1'}
+        sx={{
+          fontSize: '64px',
+          textAlign: 'center',
+          marginTop: '40px',
+        }}>
+        Шашки Чапаева
+      </Typography>
+      <Box
+        component={'button'}
+        className={'main-btn'}
+        sx={{
+          background: '#769656',
+          border: '1px solid #f8f8f8',
+          borderRadius: '20px',
+          fontSize: '32px',
+          padding: '5px 10px',
+          marginTop: '20px',
+          cursor: 'pointer',
+        }}
+        onClick={handleClick}>
+        Начать игру
+      </Box>
+    </Box>
+  )
 }
 
 export default MainPage
