9e3a573c3028c347846b825dd67137b8e0c898c0
Alex <ddragon99999@gmail.com>
2023-10-23 15:33:57 +0300

    [page] end demo

--- packages/client/src/pages/End/EndPage.tsx
+++ packages/client/src/pages/End/EndPage.tsx
@@ -1,7 +1,30 @@
 import React from 'react'
+import { Box, Typography } from '@mui/material'
+import MainButton from '../../components/MainButton'
 
 const EndPage = () => {
-  return <div>EndPage</div>
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+      className={'container'}>
+      <Typography
+        variant={'h1'}
+        component={'h1'}
+        sx={{
+          fontSize: '64px',
+          textAlign: 'center',
+          marginTop: '40px',
+        }}>
+        Игра закочена
+      </Typography>
+      <MainButton to={'/start'} text={'Продолжить'} />
+      <MainButton to={'/'} text={'Главное меню'} />
+    </Box>
+  )
 }
 
 export default EndPage
