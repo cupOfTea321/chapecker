20a9c901a316b43b11f748090751a4b84830e7cd
WilhelmYakunin <v.jakunin@inbox.ru>
2023-10-23 21:00:30 +0300

    feat: start coding page

--- packages/client/src/pages/Profile/ProfilePage.tsx
+++ packages/client/src/pages/Profile/ProfilePage.tsx
@@ -1,7 +1,26 @@
 import React from 'react'
 
+import bem from 'bem-ts'
+import './styles.scss'
+import { IProfileFormInputProps } from './model'
+
 const ProfilePage = () => {
-  return <div>ProfilePage</div>
+  const cn = bem('profile')
+
+  return (
+    <div className={cn()}>
+      <h1>User profile page</h1>
+      <label></label>
+    </div>
+  )
+}
+
+const LabeledInput = ({ name }: IProfileFormInputProps) => {
+  return (
+    <label htmlFor={name}>
+      <input id={name} name={name} title={name} />
+    </label>
+  )
 }
 
 export default ProfilePage
---
+++ packages/client/src/pages/Profile/model.ts
@@ -0,0 +1,13 @@
+export enum ProfileFormFileds {
+  first_name = 'First name',
+  second_name = 'Second name',
+  display_name = 'display name',
+  login = 'login',
+  email = 'email',
+  phone = 'phone',
+  avatarSrc = 'Avatar',
+}
+
+export interface IProfileFormInputProps {
+  name: string
+}
