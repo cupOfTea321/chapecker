2c73e9dccb62f99b1b34d93a7137ad9f13c2595c
Кудрин Максим <kudrin.ma2012@ya.ru>
2023-10-27 22:11:08 +0300

    removed the code in app.tsx which caused error

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,20 +1,9 @@
-import { useEffect } from 'react'
 import './App.scss'
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { routes } from './router/router'
 
 function App() {
-  useEffect(() => {
-    const fetchServerData = async () => {
-      const url = `http://localhost:${__SERVER_PORT__}`
-      const response = await fetch(url)
-      const data = await response.json()
-      console.log(data)
-    }
-
-    fetchServerData()
-  }, [])
   return (
     <div className="App">
       <BrowserRouter>
