57f0f6c6e03125383089cd9ce46ab79724dbdde9
Alex <ddragon99999@gmail.com>
2023-11-04 20:11:12 +0300

    fix

--- packages/client/src/pages/Main/MainPage.tsx
+++ packages/client/src/pages/Main/MainPage.tsx
@@ -1,12 +1,9 @@
 import React from 'react'
 import { useGetUserQuery } from '../../redux/services/yandexCore'
-import { useAppDispatch } from '../../redux/store'
 import { getUser } from '../../redux/features/userSlice'
 
 const MainPage = () => {
   const { data, isFetching } = useGetUserQuery(1)
-  const dispatch = useAppDispatch()
-  // console.log(dispatch)
 
   if (!isFetching) return <div>загрузка</div>
   const user = getUser()
