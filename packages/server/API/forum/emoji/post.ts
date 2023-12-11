009c1d00534fb594c3ab1b0cc3ff8bc2bfdd7596
Alex <ddragon99999@gmail.com>
2023-12-11 19:36:49 +0300

    [back] emoji endpoints added

--- packages/server/API/forum/emoji/post.ts
+++ packages/server/API/forum/emoji/post.ts
@@ -6,24 +6,21 @@ import { Emoji } from '../../database'
 export default function postEmojiAPI(app: Express) {
   app.post(forumEmojiAddURL, async (req, res) => {
     const creator_id = await getUserId(req)
+
     if (!creator_id) {
       res.status(401).send('')
       return
     }
-    const { emoji, emoji_id } = req.body
+    const { emoji, topic_id } = req.body
     if (!emoji) {
       res.status(400).send('Пустой emoji')
-      return
-    }
-    if (!emoji_id) {
-      res.status(400).send('Не указан emoji_id')
       return
     }
     try {
       await Emoji.create({
         creator_id,
-        emoji_id,
         emoji,
+        topic_id,
       })
       res.status(201).send('OK')
     } catch (e) {
