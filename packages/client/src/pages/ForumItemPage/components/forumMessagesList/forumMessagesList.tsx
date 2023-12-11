230647febe3bf7d9487f0c4d6147720e27e157e0
Alex <ddragon99999@gmail.com>
2023-12-11 14:14:26 +0300

    [feature] emoji and user forum name added

--- packages/client/src/pages/ForumItemPage/components/forumMessagesList/forumMessagesList.tsx
+++ packages/client/src/pages/ForumItemPage/components/forumMessagesList/forumMessagesList.tsx
@@ -1,11 +1,17 @@
-import { memo } from 'react'
+import { memo, useState } from 'react'
 import { TMessage } from '../../model'
 
 import bem from 'bem-ts'
 import './styles.scss'
 import { forums } from '../../../Forum/components/forumPreviewTable/stubs'
 
+const emoji = ['😀', '🤑', '😭', '💩', '❤', '👍']
 const ForumMessagesList = ({ messages }: { messages: TMessage[] }) => {
+  const [selectedEmoji, setSelectedEmoji] = useState('')
+
+  const handleEmojiSelect = (emoji: string) => {
+    setSelectedEmoji(emoji)
+  }
   const cn = bem('forumMessages')
   const getTime = (time: Date) => {
     return (
@@ -29,7 +35,26 @@ const ForumMessagesList = ({ messages }: { messages: TMessage[] }) => {
             <span>{author}</span>
             <span>{getTime(time)}</span>
           </div>
+          {selectedEmoji}
           <p>{message}</p>
+          <div
+            style={{
+              display: 'flex',
+            }}>
+            {emoji.map((item, index) => (
+              <div>
+                <button
+                  key={item}
+                  style={{
+                    width: '30px',
+                    height: '30px',
+                  }}
+                  onClick={() => handleEmojiSelect(`${item}`)}>
+                  {item}
+                </button>
+              </div>
+            ))}
+          </div>
         </li>
       ))}
     </ul>
